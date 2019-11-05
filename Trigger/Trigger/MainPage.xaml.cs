f6c2848f565107c9c7cb10bfebcf2bda35f221a5
Leonid <leonid0@hotmail.es>
2019-11-04 23:31:19 -0500

    Agregar archivos de proyecto.

---
+++ Trigger/Trigger/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Trigger
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Trigger.Data());
+
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Trigger.Event());
+            };
+
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Trigger.Property());
+
+            };
+            Item5.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Trigger.Style());
+
+            };
+
+            Item6.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Trigger.MultiTrigger());
+
+            };
+
+        }
+    }
+}
